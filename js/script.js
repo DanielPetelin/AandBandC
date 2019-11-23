cfd29bf34ddaaaef44d6072aa61872f8fd75ebcd
Daniil Petelin <DaniilPetelin77@gmail.com>
2019-11-23 17:10:47 +0300

    A, B, C - рабочие сравнение чисел

--- js/script.js
+++ js/script.js
@@ -1,8 +1,18 @@
 var A = 12;
-var B = 8;
+var B = 2;
+var C = 6;
 
 if (A > B) {
-        console.log('A');
+        if (A > C) {
+            console.log('A');
+        } else {
+            console.log('C');
+        }
     } else {
-        console.log('B');
-    };
+        if (B > C) {
+            console.log('B')
+        }
+        else {
+            console.log('C')
+        }
+    }
