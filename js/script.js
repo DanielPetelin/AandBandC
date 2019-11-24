00a76178c8a43774743987cfbf66df7748db4f04
Daniil Petelin <DaniilPetelin77@gmail.com>
2019-11-24 15:41:02 +0300

    4 буквы и равенство

--- js/script.js
+++ js/script.js
@@ -1,18 +1,28 @@
-var A = 12;
-var B = 2;
-var C = 6;
+var A = 51;
+var B = 92;
+var C = 3;
+var D = 12;
 
 if (A > B) {
-        if (A > C) {
-            console.log('A');
-        } else {
-            console.log('C');
-        }
+    if (A > C) {
+        if (A > D) {
+        console.log('A');
+    } else {
+        console.log('D');
+    }
+}
     } else {
         if (B > C) {
+            if (B > D) {
             console.log('B')
-        }
-        else {
-            console.log('C')
-        }
+        } else {
+            console.log('D')   
+        } 
+    } else {
+        console.log('C')
     }
+}
+
+if (A === B || A === C || A === D || B === C || C === D) {
+    console.log('равные элементы')
+}
